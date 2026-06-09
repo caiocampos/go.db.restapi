c58246699b3d10121f29b2d2f082bb891a4ed7dc
Caio Campos <20524962+caiocampos@users.noreply.github.com>
2026-06-09 15:01:12 -0300

    updated parsley

--- modules/controller_module.go
+++ modules/controller_module.go
@@ -1,14 +1,18 @@
 package modules
 
 import (
+	"context"
+
 	"github.com/matzefriedrich/parsley/pkg/features"
 	"github.com/matzefriedrich/parsley/pkg/types"
 	"go.db.restapi/controller"
 )
 
 func ConfigureControllers(registry types.ServiceRegistry) error {
+	ctx := context.Background()
+	
 	registry.Register(controller.NewUserController, types.LifetimeTransient)
-	features.RegisterList[controller.Controller](registry)
+	features.RegisterList[controller.Controller](ctx, registry)
 
 	return nil
 }
--- modules/repository_module.go
+++ modules/repository_module.go
@@ -1,6 +1,8 @@
 package modules
 
 import (
+	"context"
+
 	"github.com/matzefriedrich/parsley/pkg/features"
 	"github.com/matzefriedrich/parsley/pkg/types"
 	"go.db.restapi/model"
@@ -8,11 +10,13 @@ import (
 )
 
 func ConfigureRepositories(registry types.ServiceRegistry) error {
+	ctx := context.Background()
+
 	registry.Register(repository.NewUserMongoRepository, types.LifetimeTransient)
 
 	registry.Register(repository.NewUserRedisRepository, types.LifetimeTransient)
 	registry.Register(repository.NewUserValkeyRepository, types.LifetimeTransient)
-	features.RegisterList[repository.UserCacheRepository[model.User]](registry)
+	features.RegisterList[repository.UserCacheRepository[model.User]](ctx, registry)
 
 	return nil
 }
